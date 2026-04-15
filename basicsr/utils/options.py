60fc5a47fd8460a087f40c0ab9c998f9ddc09657
Alper <121297476+alperslmz@users.noreply.github.com>
2026-04-15 16:58:11 +0300

    Update options.py
    to save experiments to Google Drive

--- basicsr/utils/options.py
+++ basicsr/utils/options.py
@@ -155,7 +155,11 @@ def parse_options(root_path, is_train=True):
             opt['path'][key] = osp.expanduser(val)
 
     if is_train:
-        experiments_root = osp.join(root_path, 'experiments', opt['name'])
+        if opt['path'].get('experiments_root'):
+            experiments_root = opt['path']['experiments_root']
+        else:
+            experiments_root = osp.join(root_path, 'experiments', opt['name'])
+        experiments_root = osp.join(experiments_root, opt['name'])
         opt['path']['experiments_root'] = experiments_root
         opt['path']['models'] = osp.join(experiments_root, 'models')
         opt['path']['training_states'] = osp.join(experiments_root, 'training_states')
